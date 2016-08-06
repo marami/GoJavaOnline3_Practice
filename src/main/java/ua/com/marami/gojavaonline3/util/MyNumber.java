918c1545e3f76363fef1a8cf74deb0abc7afb4e0
Marina Dobritsa <maramister@gmail.com>
2016-08-06 23:16:17 +0300

    create method numberToArray() with tests

--- src/main/java/ua/com/marami/gojavaonline3/util/MyNumber.java
+++ src/main/java/ua/com/marami/gojavaonline3/util/MyNumber.java
@@ -4,10 +4,15 @@ public class MyNumber {
 
 	public static String intArrayToString(int[] numbers) {
 		final StringBuilder builder = new StringBuilder();
-		for (int number: numbers) {
+		for (int number : numbers) {
 			builder.append(number);
 		}
 		return builder.toString();
+	}
+
+	public static int[] numberToArray(int number) {
+		int firstIndex = number >= 0 ? 0 : 1;
+		return Integer.toString(number).substring(firstIndex).chars().map(c -> c -= '0').toArray();
 	}
 
 }
---
+++ src/test/java/ua/com/marami/gojavaonline3/util/MyNumberParametrizedTestForNumberToArray.java
@@ -0,0 +1,43 @@
+package ua.com.marami.gojavaonline3.util;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(value = Parameterized.class)
+public class MyNumberParametrizedTestForNumberToArray {
+
+	@Parameter
+	public int fInput;
+
+	@Parameter(value = 1)
+	public int[] fExpected;
+
+	@Parameters
+	public static Collection<Object[]> getParametrizedData() {
+		return Arrays.asList(new Object[][]{
+				{22, new int[]{2, 2}},
+				{0, new int[]{0}},
+				{-1, new int[]{1}},
+				{-954, new int[]{9, 5, 4}}
+		});
+	}
+
+	@Rule
+	public final Timeout globalTimeout = Timeout.seconds(1);
+
+	@Test
+	public void numberToArray() throws Exception {
+		assertArrayEquals(fExpected, MyNumber.numberToArray(fInput));
+	}
+
+}
