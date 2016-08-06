0a257f73f5fd3e9b2e79d92e90c45991b61a73aa
Marina Dobritsa <maramister@gmail.com>
2016-08-06 16:58:58 +0300

    call task1 from main()

--- src/ua/com/marami/gojavaonline3/Main.java
+++ src/ua/com/marami/gojavaonline3/Main.java
@@ -1,8 +1,17 @@
 package ua.com.marami.gojavaonline3;
 
+import ua.com.marami.gojavaonline3.dao.ConsoleReader;
+import ua.com.marami.gojavaonline3.dao.Reader;
+import ua.com.marami.gojavaonline3.view.ConsoleWriter;
+import ua.com.marami.gojavaonline3.view.Writer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Reader reader = new ConsoleReader();
+        Writer writer = new ConsoleWriter();
+
+        Task1 task1 = new Task1(reader, writer);
+        task1.run();
     }
 }
