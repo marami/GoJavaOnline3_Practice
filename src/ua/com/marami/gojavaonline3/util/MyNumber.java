d1679a248b6e2b2580f5549f0c0c5e55d3b225e8
Marina Dobritsa <maramister@gmail.com>
2016-08-06 16:13:13 +0300

    create MyNumber.java with intArrayToString()

---
+++ src/ua/com/marami/gojavaonline3/util/MyNumber.java
@@ -0,0 +1,13 @@
+package ua.com.marami.gojavaonline3.util;
+
+public class MyNumber {
+
+	public String intArrayToString(int[] numbers) {
+		final StringBuilder builder = new StringBuilder();
+		for (int number: numbers) {
+			builder.append(number);
+		}
+		return builder.toString();
+	}
+
+}
