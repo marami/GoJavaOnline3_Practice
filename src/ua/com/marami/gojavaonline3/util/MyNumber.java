1b125cc02f1a716fe4c9a964f0234af246db35a2
Marina Dobritsa <maramister@gmail.com>
2016-08-06 16:47:43 +0300

    change method to static

--- src/ua/com/marami/gojavaonline3/util/MyNumber.java
+++ src/ua/com/marami/gojavaonline3/util/MyNumber.java
@@ -2,7 +2,7 @@ package ua.com.marami.gojavaonline3.util;
 
 public class MyNumber {
 
-	public String intArrayToString(int[] numbers) {
+	public static String intArrayToString(int[] numbers) {
 		final StringBuilder builder = new StringBuilder();
 		for (int number: numbers) {
 			builder.append(number);
